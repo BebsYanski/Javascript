b5901db095562352cbb9c0523cfd2bfca33ee36b
BebsYanski <bebongnchuy@gmail.com>
2022-12-14 11:12:53 +0100

    Arithmetic operations added.

---
+++ arithmeticOperators.js
@@ -0,0 +1,20 @@
+// arithmetic operators here are:
+// +, -, *, /, %, **, ++, --
+
+// console.log(1 + 1);
+// console.log(1 - 1);
+// console.log(1 * 2);
+// console.log(4 / 2);
+// console.log(5 % 3);
+// console.log(2 ** 4);
+
+// let number = 5;
+// console.log(number++);
+// console.log(++number);
+// console.log(number--);
+// console.log(--number);
+
+let age = 23;
+console.log(age);
+console.log(age++, age);
+console.log(1 + 1 + "2");
